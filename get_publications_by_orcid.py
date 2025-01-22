6e93d21dd098904d4231f1c6312205b13719360e
Sujit Silas Armstrong Suthahar <70657722+sujitsilas@users.noreply.github.com>
2025-01-22 15:25:50 -0800

    Update get_publications_by_orcid.py

--- get_publications_by_orcid.py
+++ get_publications_by_orcid.py
@@ -2,7 +2,7 @@ import requests
 import yaml
 
 # Your ORCID ID
-orcid_id = "0009-0000-7683-5299"
+orcid_id = "0000-0002-2538-4691"
 # ORCID API endpoint for public data
 
 # Set headers for the API request
